d2bb6c81637780061ae62df15e9c23f135a69790
Sasni MRM <sasni.20200168@iit.ac.lk>
2023-06-17 05:01:36 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+    const { username } = req.body;
+
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username },
+            { headers: { "private-key": "c26f83ca-6ac1-4e8d-ae0f-2f8573dab547" } }
+        )
+        return res.status(r.status).json(r.data)
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data)
+    }
 });
 
 app.listen(3001);
