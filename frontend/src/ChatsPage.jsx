a0b421773a9bb24ae83ba29012b54a40e7a6971c
Sasni MRM <sasni.20200168@iit.ac.lk>
2023-06-17 06:21:14 +0530

    chat done

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return (
+        <div style={{ height: '100vh' }}>
+            <PrettyChatWindow
+                projectId='4198750c-b65c-4984-9883-c62a41a0f3bf'
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%' }}>
+            </PrettyChatWindow>
+        </div>
+    )
 }
 
 export default ChatsPage
