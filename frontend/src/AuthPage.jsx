11ec477d25ff476445666c75ece5ed7202f1a6bd
Sasni MRM <sasni.20200168@iit.ac.lk>
2023-06-17 05:50:07 +0530

    authpage

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
       e.preventDefault();
       const { value } = e.target[0];
       axios.post(
-        'http://localhost:3001/authanticate',
+        'http://localhost:3001/authenticate',
         {username: value}
         )
         .then(r => props.onAuth({ ...r.data, secret: value }))
